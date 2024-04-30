7444049f3db6543986f803454128940d04afded6
Harshit <101876400+imaoharsh@users.noreply.github.com>
2024-05-01 02:25:56 +0530

    finals3

--- app.py
+++ app.py
@@ -106,32 +106,8 @@ if page == "Home":
 
     data_training_array=scaler.fit_transform(data_training)
 
-        
-
-
-
-    past_100_days=data_training.tail(100)
-
-    final_df=past_100_days.append(data_testing,ignore_index=True)
-
-    input_data=scaler.fit_transform(final_df)
-
-    x_test=[]
-    y_test=[]
-
-    for i in range(100,input_data.shape[0]):
-        x_test.append(input_data[i-100:i])
-        y_test.append(input_data[i,0])
-        
-    x_test,y_test=np.array(x_test),np.array(y_test)
-
-    y_predicted=model.predict(x_test)
-
-    scaler=scaler.scale_
-
-    scale_factor=1/scaler[0]
-    y_predicted=y_predicted*scale_factor
-    y_test=y_test *scale_factor
+    #ML    
+    
 
   
 
